f87a1fbf8c213d19fe8aae4e8d0c1e7f243b5cb5
GaryB93 <garybalogh93@gmail.com>
2025-09-23 10:35:15 -0500

    adds react-router and introduces routes

--- omega_crm/src/App.tsx
+++ omega_crm/src/App.tsx
@@ -1,15 +1,23 @@
 import { useState } from 'react';
+import { Routes, Route } from 'react-router';
 import './App.css';
 import Login from './pages/Login/Login';
+import DashboardLayout from './pages/Dashboard/Dashboard';
 
 function App() {
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
-    <>
-      <Login/>
-    </>
+    <Routes>
+      <Route path="login" element={<Login/>} />
+
+      <Route element={<DashboardLayout />} >
+        <Route path="schedule" element={<Schedule />} />
+        {/* <Route path="customers" element={<Customers />} />
+        <Route path="acctmgmt" element={<AccountManagement />} /> */}
+      </Route>
+    </Routes>
   )
 }
 
---
+++ omega_crm/src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,9 @@
+import { Outlet } from "react-router";
+
+function DashboardLayout () {
+  return (
+    <h1>This works</h1>
+  )
+}
+
+export default DashboardLayout;
