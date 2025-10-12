d5edeaace7b1ed80acffa1d4c151227ac4b1c8df
GaryB93 <garybalogh93@gmail.com>
2025-10-12 10:15:30 -0500

    documents a design element that makes the application scalable for Task B

--- omega_crm/src/pages/Dashboard/Dashboard.tsx
+++ omega_crm/src/pages/Dashboard/Dashboard.tsx
@@ -3,7 +3,10 @@ import './Dashboard.css';
 import Nav from "./Nav/Nav";
 import ScheduleProvider from "../../contextProviders/ScheduleProvider";
 import CustomerProvider from "../../contextProviders/CustomerProvider";
-import type { User } from "../AcctMgmt/UserInfo/UserInfo";
+import User from "../../classes/User";
+
+// This dashboard component is the main container that houses the navigation component and the pages of the application that pertain to individual functionalities.
+// This will allow for the easy addition of new functionalities as the application is being developed into a full CRM application.
 
 interface DashboardLayoutProps {
   setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
